87b5d6d2a9fbe26822e74a0ac3ce0b087889bdf2
Mehmet Uysal <mehmetuysal2000@yandex.com>
2022-12-01 15:25:48 +0300

    0.0.3
    HomeView page edited(Explanation added about my own work folder stracture)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="h-screen grid grid-cols-2">  
+  <div class="h-screen grid grid-cols-2 text-xl">
+    <div class="flex mt-10 col-span-2 border">
+      <div class="m-auto">
+        While I am working I used this folder structure
+        <div>-->quickqarbon</div>
+        <div>---->backend</div>
+        <div>---->frontend</div>
+      </div>
+    </div>
     <RouterLink class="m-auto" to="/frontend">Frontend</RouterLink>
     <RouterLink class="m-auto" to="/backend">Backend</RouterLink>
     <RouterLink class="m-auto" to="/backenddocs">Backend Documentation</RouterLink>

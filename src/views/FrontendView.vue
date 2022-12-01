f15cc224663adb566125f7fdaf31c16eea61684e
Mehmet Uysal <mehmetuysal2000@yandex.com>
2022-12-01 15:48:55 +0300

    0.0.4
    Explanation added to FrontedView.vue

--- src/views/FrontendView.vue
+++ src/views/FrontendView.vue
@@ -1,6 +1,30 @@
 <template>
   <div class="w-screen h-screen flex flex-wrap">
-    <div class="w-full m-auto mb-5 text-center">function writes results to console</div>
+    <div class="w-full m-auto mb-5">
+      <p class="px-5">
+        This buttons runs `waitForPromise` (from `frontend/src/services/web/promiseAwaitService.ts`)<br />
+        and `apiRequestF` (from `frontend/src/services/api/api.ts`) <br />
+        `waitForPromise`function gets `promise` which comes from fetch by `apiRequestF`
+        then gives result like
+      </p>
+      <details class="px-5">
+        <summary>
+          Result
+        </summary>
+        <code>
+          <pre>
+  {
+    status: status code, 
+    statusText: status text,
+    body: response
+  }
+            </pre>
+          </code>
+      </details>
+      <p class="px-5">
+        codes can be tested with buttons which are below
+      </p>
+    </div>
     <button class="m-auto mt-5 border p-5" @click="runFunction">Run function Corect</button>
     <button class="m-auto mt-5 border p-5" @click="runFunctionW">Run function Wrong</button>
   </div>
